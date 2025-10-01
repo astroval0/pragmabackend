acf81f48cdd5b102f1e617f2f2d301f88692b6df
Astro <astroval0@proton.me>
2025-10-01 15:55:01 +0000

    Update HTTP response to use HTTP/2 and improve headers
    Updated the HTTP response version to HTTP/2 by setting `res.version(20)`. Removed the `Server` header (`FakePragma`) and updated the `Content-Type` header to include `charset=UTF-8`. Added a `Vary` header with the value `Origin` to indicate response variability based on the `Origin` request header. Retained the existing response handling logic.

--- Packets/Implementation/StaticResponseProcessorHTTP.cpp
+++ Packets/Implementation/StaticResponseProcessorHTTP.cpp
@@ -1,11 +1,12 @@
 #include <StaticResponseProcessorHTTP.h>
 
 void StaticResponseProcessorHTTP::Process(http::request<http::string_body> const& req, tls_stream& sock) {
-	http::response<http::string_body> res;
-	res.set(http::field::server, "FakePragma");
-	res.set(http::field::content_type, "application/json");
-	res.result(http::status::ok);
-	res.body() = m_res->dump();
-	res.prepare_payload();
-	http::write(sock, res);
+    http::response<http::string_body> res;
+    res.version(20); // HTTP/2
+    res.result(http::status::ok);
+    res.set(http::field::content_type, "application/json; charset=UTF-8");
+    res.set(http::field::vary, "Origin");
+    res.body() = m_res->dump();
+    res.prepare_payload();
+    http::write(sock, res);
 }
