b5347245cb5d966a1fe842d209c9ff14011ca0a2
Astro <astroval0@proton.me>
2025-10-02 00:12:13 +0000

    update hooking to use header var, also implement spectre exe finding with steam dir reg key

--- Patcher/headers/globals.h
+++ Patcher/headers/globals.h
@@ -9,6 +9,7 @@ extern APlayerController* gPlayerController;
 constexpr uintptr_t RVA_EngineInit = 0x1A11AE0;
 constexpr auto RVA_C2Dispatcher = 0x63F0FC0;
 constexpr auto RVA_ConsoleCommand = 0x5B500B0;
+constexpr auto RVA_TickActorInput = 0x5B51070;
 
 static constexpr uintptr_t RVA_Serialize = 0x392F5C0;
 static constexpr uintptr_t RVA_LogCmd = 0x38D4960;
--- Patcher/hooking.cpp
+++ Patcher/hooking.cpp
@@ -1,16 +1,16 @@
 #include "headers/hooking.h"
 #include "headers/decrypt.h"
+#include "headers/globals.h"
 #include "headers/consolecmd.h"
 #include <iostream>
 
 static SafetyHookInline TickHook;
 static bool pcCaptured = false;
 
-constexpr auto RVA_TickActorInput = 0x5B51070; //todo move ts to a header
 __int64 __fastcall TickHookFn(APlayerController* pc, void* unused, float deltaSeconds) {
     // lightweight keepalive
     DecryptPage(reinterpret_cast<void *>(BaseAddress + RVA_TickActorInput));
-    DecryptPage(reinterpret_cast<void *>(BaseAddress + 0x5B500B0)); //todo use header var
+    DecryptPage(reinterpret_cast<void *>(BaseAddress + RVA_ConsoleCommand)); 
 
     static int counter = 0;
     if (++counter >= 600) {
