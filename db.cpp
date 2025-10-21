16a9d042cad482219e4b63a616545d7bb806b853
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-10-20 21:20:48 -0400

    Remove sample code from db.cpp

--- db.cpp
+++ db.cpp
@@ -9,19 +9,4 @@ namespace fs = std::filesystem;
 void SetupDatabase() {
 	PlayerDatabase playerData(fs::path("playerdata.sqlite"));
 	playerData.AddPrototype(FieldKey::PLAYER_INGAME_NAME, "PlayerName");
-	std::unique_ptr<pbuf::Message> newField = playerData.CreateObjectOfFieldType(FieldKey::PLAYER_INGAME_NAME);
-	std::unique_ptr<PlayerName> newName(static_cast<PlayerName*>(newField.release()));
-	spdlog::info("nameinit: {}", newName->name()); // this throws, something is wrong with CreateObjectOfFieldType function
-	PlayerName name;
-	name.set_name("myname");
-	sql::Statement statement = playerData.FormatStatement(
-		"INSERT INTO {table} ({col}) VALUES(?)", FieldKey::PLAYER_INGAME_NAME
-	);
-	playerData.SetField(statement, FieldKey::PLAYER_INGAME_NAME, &name);
-	sql::Statement fetchStatement = playerData.FormatStatement(
-		"SELECT {col} FROM {table} LIMIT 1", FieldKey::PLAYER_INGAME_NAME
-	);
-	std::unique_ptr<pbuf::Message> nameFetched = playerData.GetField(fetchStatement, FieldKey::PLAYER_INGAME_NAME);
-	std::unique_ptr<PlayerName> converted(static_cast<PlayerName*>(nameFetched.release()));
-	spdlog::info("name: {}", converted->name());
 }
