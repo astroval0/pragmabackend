e26a66de0ee81fb876680be7079a9466a40d3a83
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-10-28 10:42:38 -0400

    fix bug with saving loadouts

--- Packets/Implementation/SaveWeaponLoadoutProcessor.cpp
+++ Packets/Implementation/SaveWeaponLoadoutProcessor.cpp
@@ -9,25 +9,26 @@ SaveWeaponLoadoutProcessor::SaveWeaponLoadoutProcessor(SpectreRpcType rpcType) :
 }
 
 void SaveWeaponLoadoutProcessor::Process(SpectreWebsocketRequest& packet, SpectreWebsocket& sock) {
-	const WeaponLoadout& dataToSave = packet.GetPayloadAsMessage<SaveWeaponLoadoutMessage>()->weaponloadoutdata();
+	std::unique_ptr<SaveWeaponLoadoutMessage> dataToSave = packet.GetPayloadAsMessage<SaveWeaponLoadoutMessage>();
+	const WeaponLoadout& loadoutToSave = dataToSave->weaponloadoutdata();
 	sql::Statement getLoadout = PlayerDatabase::Get().FormatStatement(
 		"SELECT {col} from {table} WHERE PlayerId = ?",
 		FieldKey::PLAYER_WEAPON_LOADOUT
 	);
-	std::string toFindLoadoutId = dataToSave.loadoutid();
+	std::string toFindLoadoutId = loadoutToSave.loadoutid();
 	std::transform(toFindLoadoutId.begin(), toFindLoadoutId.end(), toFindLoadoutId.begin(),
-		[](unsigned char c) {return std::tolower(c); }); // now getting error every request, to investigate
+		[](unsigned char c) {return std::tolower(c); });
 	std::unique_ptr<WeaponLoadouts> loadouts = PlayerDatabase::Get().GetField<WeaponLoadouts>(getLoadout, FieldKey::PLAYER_WEAPON_LOADOUT);
 	bool dataWritten = false;
 	for (int i = 0; i < loadouts->weaponloadoutdata_size(); i++) {
 		if (loadouts->weaponloadoutdata(i).loadoutid() == toFindLoadoutId) {
-			loadouts->mutable_weaponloadoutdata(i)->CopyFrom(dataToSave);
+			loadouts->mutable_weaponloadoutdata(i)->CopyFrom(loadoutToSave);
 			dataWritten = true;
 			break;
 		}
 	}
 	if (!dataWritten) {
-		loadouts->add_weaponloadoutdata()->CopyFrom(dataToSave);
+		loadouts->add_weaponloadoutdata()->CopyFrom(loadoutToSave);
 	}
 	sql::Statement setLoadout = PlayerDatabase::Get().FormatStatement(
 		"INSERT OR REPLACE INTO {table} (PlayerId, {col}) VALUES (?, ?)",
@@ -37,6 +38,6 @@ void SaveWeaponLoadoutProcessor::Process(SpectreWebsocketRequest& packet, Spectr
 	PlayerDatabase::Get().SetField(setLoadout, FieldKey::PLAYER_WEAPON_LOADOUT, loadouts.get(), 2);
 	std::shared_ptr<json> res = packet.GetBaseJsonResponse();
 	(*res)["payload"]["success"] = true;
-	(*res)["payload"]["savedLoadoutId"] = dataToSave.loadoutid();
+	(*res)["payload"]["savedLoadoutId"] = loadoutToSave.loadoutid();
 	sock.SendPacket(res);
 }
